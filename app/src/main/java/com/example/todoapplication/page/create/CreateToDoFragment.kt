91322b0a81cc185642bce176706d5b75213098a2
tayuta914 <tayuta914@gmail.com>
2024-03-25 00:29:25 +0900

    Add CreateToDoScreenm

--- app/src/main/java/com/example/todoapplication/page/create/CreateToDoFragment.kt
+++ app/src/main/java/com/example/todoapplication/page/create/CreateToDoFragment.kt
@@ -3,22 +3,61 @@ package com.example.todoapplication.page.create
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.application.R
+import com.example.application.databinding.CreateTodoFragmentBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CreateToDoFragment:Fragment(R.layout.create_todo_fragment) {
     private val vm: CreateToDoViewModel by viewModels()
 
+    private var _binding: CreateTodoFragmentBinding? = null
+    private val binding: CreateTodoFragmentBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this._binding = CreateTodoFragmentBinding.bind(view)
+
+        vm.errorMessage.observe(viewLifecycleOwner) { msg ->
+            Snackbar.make(requireView(), msg, Snackbar.LENGTH_SHORT).show()
+            vm.errorMessage.value = ""
+        }
+        vm.done.observe(viewLifecycleOwner) {
+            findNavController().popBackStack()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.menu_create, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_save -> {
+                save()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun save() {
+        val title = binding.titleEdit.text.toString()
+        val detail = binding.detailEdit.text.toString()
+
+        vm.save(title, detail)
+    }
+
 }

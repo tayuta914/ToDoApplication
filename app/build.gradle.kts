3da33b46d0956cf710c1f9727f7841db75193db2
tayuta914 <tayuta914@gmail.com>
2025-01-19 21:06:17 +0900

    Update compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "com.example.application"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.application"

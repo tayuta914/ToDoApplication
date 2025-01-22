cf25771f4f958d1e95fd68468cd947a6ceabcca9
tayuta914 <tayuta914@gmail.com>
2025-01-23 00:42:59 +0900

    Add detail_screen transition

--- app/src/main/java/com/example/todoapplication/page/deteail/ToDoDetailFragment.kt
+++ app/src/main/java/com/example/todoapplication/page/deteail/ToDoDetailFragment.kt
@@ -1,9 +1,13 @@
 package com.example.todoapplication.page.deteail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.application.R
 import com.example.application.databinding.TodoDetailFragmentBinding
@@ -18,6 +22,11 @@ class ToDoDetailFragment:Fragment(R.layout.todo_detail_fragment) {
 
     private val args: ToDoDetailFragmentArgs by navArgs()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -31,5 +40,23 @@ class ToDoDetailFragment:Fragment(R.layout.todo_detail_fragment) {
     override fun onDestroyView() {
         super.onDestroyView()
         this._binding = null
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu:Menu, inflater:MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_detail ,menu)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_edit -> {
+                val action = ToDoDetailFragmentDirections.actionToDoDetailFragmentToEditToDoFragment(args.todo)
+                findNavController().navigate(action)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/todoapplication/page/edit/EditToDoFragment.kt
+++ app/src/main/java/com/example/todoapplication/page/edit/EditToDoFragment.kt
@@ -1,11 +1,48 @@
 package com.example.todoapplication.page.edit
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.application.R
+import com.example.application.databinding.EditTodoFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EditToDoFragment:Fragment(R.layout.edit_todo_fragment) {
+    private val args: EditToDoFragmentArgs by navArgs()
+
     private val vm: EditToDoViewModel by viewModels()
+
+    private var _binding: EditTodoFragmentBinding? =  null
+    private val binding: EditTodoFragmentBinding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this._binding = EditTodoFragmentBinding.bind(view)
+
+        // EditTextの初期値をセット
+        val todo = args.todo
+        binding.titleEdit.setText(todo.title)
+        binding.detailEdit.setText(todo.detail)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        this._binding = null
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_edit, menu)
+    }
 }
